51535da34be7894742830f11d4bbe0e8e27a46d3
idleberg <jan@idleberg.com>
2018-05-23 20:30:39 +0200

    remove unused tasks

--- gulpfile.js
+++ gulpfile.js
@@ -22,32 +22,27 @@ const jsonFiles = [
     './package.json'
 ];
 
-const lessFiles = [
-    './styles/**/*.less'
-];
-
 // Lint CoffeeScript & CSON files
-gulp.task('lint:coffee', gulp.series(function(done) {
+gulp.task('lint:coffee', (done) => {
     gulp.src(coffeeFiles)
         .pipe(debug({title: 'coffeelint:'}))
         .pipe(coffeelint())
         .pipe(coffeelint.reporter(stylish))
         .pipe(coffeelint.reporter('fail'));
     done();
-}));
+});
 
 // Lint JSON files
-gulp.task('lint:json', gulp.series(function(done) {
+gulp.task('lint:json', (done) => {
     gulp.src(jsonFiles)
         .pipe(debug({title: 'jsonlint:'}))
         .pipe(jsonlint())
         .pipe(jsonlint.failAfterError())
         .pipe(jsonlint.reporter());
     done();
-}));
+});
 
 // Tasks
-gulp.task('lint', gulp.parallel('lint:coffee', 'lint:json', function(done) {
+gulp.task('lint', gulp.parallel('lint:coffee', 'lint:json', done => {
   done();
 }));
-
